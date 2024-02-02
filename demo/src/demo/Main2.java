335c86c7cda20c4adc57c97ae819381111841625
Admin <Admin@192.168.1.44>
2024-02-02 21:41:01 +0530

    update

---
+++ demo/src/demo/Main2.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Main2 {
+public static void main(String[] args) {
+	System.out.println("Jayesh Mistry");
+	System.out.println("Jayesh Mistry");
+	System.out.println("Jayesh Mistry");
+	System.out.println("Jayesh Mistry");
+	System.out.println("Jayesh Mistry");
+}
+}
