acb2cf5624028efebca4f0b7bdd562a8c2bd0e65
Admin <Admin@192.168.1.44>
2024-02-02 21:38:09 +0530

    push and commit

---
+++ demo/src/demo/Main.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Main {
+public static void main(String[] args) {
+	System.out.println("hello world");
+	System.out.println("hello world");
+	System.out.println("hello world");
+	System.out.println("hello world");
+}
+}
